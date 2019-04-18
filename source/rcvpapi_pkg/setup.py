075e048b3fd5006a76bf470805bb36cab0b95b71
Rob Martin <robmartin@arista.com>
2019-04-18 14:14:51 -0500

    Created python package struct

---
+++ source/rcvpapi_pkg/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="rcvp_api",
+    version="1.0",
+    description="Module to interact with Arista CloudVision",
+    url="https://github.com/networkRob/cvp-api",
+    author="Rob Martin",
+    author_email="robmartin@arista.com",
+    packages=['rcvp_api'],
+    zip_safe=False)
